f8b46bdfe12e7e4da28be8c477392f782f954ec6
Marta Soler Boada <marta.soler00@gmail.com>
2023-01-11 15:30:38 +0100

    Added comments

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -7,12 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ClockComponent {
 
-  // Displayed dated
+  // Displayed date
   date: any;
+
+  // User's time to add
   hoursAdd: number = 0;
   minutesAdd: number = 0;
   
-  // Intervals to display dinamically 
+  // Interval to display dinamically 
   interval: any 
 
   // Modes variables
@@ -20,50 +22,80 @@ export class ClockComponent {
   modeCount : number = 0;
   ligthMode: Boolean = false;
 
+  /**
+   * Set an interval to display the current time, check it every second
+   */
   ngOnInit(){
      this.interval = setInterval(() => {
       const currentDate = new Date();
       this.date = currentDate.toLocaleTimeString();
-    }, 1000);
+    }, 1000); //ms
   }
 
+  /**
+   * Action when '+' is clicked. 
+   * It will increase the hours or the minutes depending on the numbers of times it has been clicked.
+   * 
+   */
   onAdd():void{
+    console.log("on add")
     this.modeCount = this.modeCount + 1;
+
     if (this.modeCount == 1){
       this.hoursAdd  = this.hoursAdd + 1;
     } else if (this.modeCount == 2){
       this.minutesAdd  = this.minutesAdd + 1;
-    } else {
-      this.displayOptions = false;
-      this.modeCount = 0;
+    } else { 
+      this.displayOptions = false; // Disable '+' button
+      this.modeCount = 0; // Restart count
     }
 
-    clearInterval(this.interval);
-    this.interval = setInterval(() => {
+    clearInterval(this.interval); // Clear current interval
+
+    // Start a new interval adding the hous and minutes
+    this.interval = setInterval(() => { 
       const currentDate = new Date();
       currentDate.setHours(
         currentDate.getHours()+this.hoursAdd, 
         currentDate.getMinutes()+this.minutesAdd)
       this.date = currentDate.toLocaleTimeString();
     }, 1000);
+
   }
 
+  /**
+   * Action when button 'MODE' is clicked
+   */
   onClickMode():void{
-     if (this.modeCount == 0){
-       this.displayOptions = true;
+    console.log("on mode")
+     if (this.modeCount == 0){ // Control it's the first time
+       this.displayOptions = true; // Enable '+' button
      }
    }
 
+   /**
+    * Action when button 'LIGHT' is clicked
+    * Updates the boolean ligth mode
+    */
    onLight():void{
+    console.log("on light")
     this.ligthMode = !this.ligthMode;
   }
 
+  /**
+   * Action when button 'LIGHT' is clicked
+   * Displays current date
+   */
   onReset(): void{
+    console.log("on reset")
     clearInterval(this.interval)
     this.interval = setInterval(() => {
       const currentDate = new Date();
       this.date = currentDate.toLocaleTimeString();
     }, 1000);
 
+    // Reset variables
+    this.hoursAdd = 0;
+    this.minutesAdd = 0;
   }
 }
